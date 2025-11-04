e7a91b254f8dcede496c335cf91a24770662a3a2
Josiah Shehata <jhshehata@icloud.com>
2025-11-04 08:16:11 -0700

    keyboard-layout

---
+++ ProjectSourceCode/script.js
@@ -0,0 +1,11 @@
+// const buttonContainer = document.getElementsByClassName('btn');
+// let buttonInputs = [];
+
+
+//   buttonContainer.addEventListener('click', function(event) {
+//         // Check if the clicked element is a button
+//         if (event.target.tagName === 'BUTTON') {
+//             buttonInputs.push(event.target.value);
+//             console.log(buttonInputs);
+//         }
+//     });
