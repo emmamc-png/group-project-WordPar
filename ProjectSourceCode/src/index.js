68adebce2262d552f2bc09c22384dfc70107fac1
Reeves-me <rc.organized@gmail.com>
2025-11-11 12:21:55 -0700

    Basic outline of settings page along with route to home/settings page

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -95,6 +95,10 @@ app.get('/registration', (req, res) => {
     res.render('pages/registration', { bodyClass: 'auth-page' }); //, {bodyClass: 'auth-page'} selects the body style to be used when rendering the page
 });
 
+app.get('/settings', (req, res) => {
+  res.render('pages/settings', { bodyClass: 'home-page' }); //, {bodyClass: 'auth-page'} selects the body style to be used when rendering the page
+});
+
 //Handle when users attempt to login`
 app.post('/login', async(req,res) => {
   const username=req.body.username;
